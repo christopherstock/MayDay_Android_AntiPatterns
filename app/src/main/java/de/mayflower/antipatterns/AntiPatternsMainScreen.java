69a529d47250e05d5344b61aee8067419e527400
Christopher Stock <email@christopherstock.de>
2016-03-11 17:02:11 +0100

    White bg.

--- app/src/main/java/de/mayflower/antipatterns/AntiPatternsMainScreen.java
+++ app/src/main/java/de/mayflower/antipatterns/AntiPatternsMainScreen.java
@@ -60,28 +60,12 @@
 
             return true;
         }
-/*
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.gesture, menu);
-            return true;
-        }
-*/
+
         @Override
         public boolean onOptionsItemSelected(MenuItem item)
         {
             AntiPatternsDebug.major.out( AntiPatternsMainScreen.class + "::onOptionsItemSelected()" );
 
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-/*
-            int id = item.getItemId();
-            if (id == R.id.action_settings) {
-                return true;
-            }
-*/
             return super.onOptionsItemSelected(item);
         }
 
