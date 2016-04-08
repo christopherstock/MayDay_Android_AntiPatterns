91238946294839ce8e6aa684e8f6521cee5847eb
Martin Brotzeller <martin.brotzeller@mayflower.de>
2016-04-08 17:25:11 +0200

    added test class to suite

--- app/src/test/java/de/mayflower/antipatterns/LocalTestSuite.java
+++ app/src/test/java/de/mayflower/antipatterns/LocalTestSuite.java
@@ -1,8 +1,14 @@
 package de.mayflower.antipatterns;
+import  de.mayflower.antipatterns.data.*;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({AntiPatternsPatternCountServiceTest.class, AntiPatternsMapUtilTest.class})
+@Suite.SuiteClasses(
+        {
+                AntiPatternsPatternCountServiceTest.class,
+                AntiPatternsMapUtilTest.class,
+                PatternTest.class
+        })
 public class LocalTestSuite {}
