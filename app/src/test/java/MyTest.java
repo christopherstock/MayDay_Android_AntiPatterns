e11142e66487d6a09fc0fd55e35c68f7362ca13b
Martin Brotzeller <martin.brotzeller@mayflower.de>
2016-04-08 10:36:39 +0200

    test

--- app/src/test/java/MyTest.java
+++ app/src/test/java/MyTest.java
@@ -10,6 +10,7 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import de.mayflower.antipatterns.AntiPatternsMapUtil.SORT_ORDER;
 
 public class MyTest {
 
@@ -19,23 +20,47 @@ public class MyTest {
     }
 
     @Test
-    public void MyTest_mapUtilTest_sortsIntegers() {
-        Map<Integer, Integer> unsorted = new LinkedHashMap<Integer, Integer>();
-        Map<Integer, Integer> sorted = new LinkedHashMap<Integer, Integer>();
+    public void MyTest_mapUtilTest_sortsIntegersAsc() {
         Map<Integer, Integer> result = new LinkedHashMap<Integer, Integer>();
-        unsorted.put(1,3);
-        unsorted.put(2,1);
+        Map<Integer, Integer> sorted =getSorted(SORT_ORDER.ASC);
+        Map<Integer, Integer> unsorted =getUnsorted();
+
+        result = AntiPatternsMapUtil.sortByValue(unsorted, SORT_ORDER.ASC);
+        boolean expected = result.equals(sorted);
+        assertTrue(expected);
+    }
+    @Test
+    public void MyTest_mapUtilTest_sortsIntegersDesc() {
+        Map<Integer, Integer> result = new LinkedHashMap<Integer, Integer>();
+        Map<Integer, Integer> sorted =getSorted(SORT_ORDER.DESC);
+        Map<Integer, Integer> unsorted =getUnsorted();
+
+        result = AntiPatternsMapUtil.sortByValue(unsorted, SORT_ORDER.DESC);
+        boolean expected = result.equals(sorted);
+        assertTrue(expected);
+    }
+    private Map<Integer, Integer> getSorted(SORT_ORDER order) {
+        Map<Integer, Integer> sorted = new LinkedHashMap<Integer, Integer>();
+        switch(order) {
+            case ASC:
+                sorted.put(2,1);
+                sorted.put(3,2);
+                sorted.put(1,3);
+                break;
+            case DESC:
+                sorted.put(1,3);
+                sorted.put(3,2);
+                sorted.put(2,1);
+                break;
+        }
+        return sorted;
+    }
+    private Map<Integer, Integer> getUnsorted() {
+        Map<Integer, Integer> unsorted = new LinkedHashMap<Integer, Integer>();
+        unsorted.put(1, 3);
+        unsorted.put(2, 1);
         unsorted.put(3,2);
-        sorted.put(2,1);
-        sorted.put(3,2);
-        sorted.put(1,3);
-
-        result = AntiPatternsMapUtil.sortByValue(unsorted, AntiPatternsMapUtil.SORT_ORDER.ASC);
-        boolean foo = result.equals(sorted);
-        // System.out.print("Result is "+result.toString());
-
-        assertTrue(foo);
-
+        return unsorted;
     }
 
 }
