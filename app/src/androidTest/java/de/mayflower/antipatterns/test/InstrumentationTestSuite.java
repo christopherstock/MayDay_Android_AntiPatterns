84121ad2003434ce7e4c71f376fb05964153c0f4
Christian Heldt <christian.heldt@mayflower.de>
2016-04-08 15:10:07 +0200

    Bundles tests in testsuite

---
+++ app/src/androidTest/java/de/mayflower/antipatterns/test/InstrumentationTestSuite.java
@@ -0,0 +1,8 @@
+package de.mayflower.antipatterns.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AntiPatternsPatternCountServiceTest.class, AntiPatternsMapUtilTest.class})
+public class InstrumentationTestSuite {}
