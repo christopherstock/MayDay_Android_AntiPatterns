0d090901009b94c77f77a384b5bcaf69b134c21d
Martin Brotzeller <martin.brotzeller@mayflower.de>
2016-04-08 14:09:34 +0200

    testing count service as instrumentation test instead of using mocks

---
+++ app/src/androidTest/java/AntiPatternsPatternCountServiceTest.java
@@ -0,0 +1,75 @@
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.*;
+
+import de.mayflower.antipatterns.AntiPatternsMainScreen;
+import de.mayflower.antipatterns.AntiPatternsPatternCountService;
+
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class AntiPatternsPatternCountServiceTest extends ActivityInstrumentationTestCase2<AntiPatternsMainScreen>
+{
+    private AntiPatternsMainScreen myActivity;
+    private SharedPreferences mySharedPreferences;
+    AntiPatternsPatternCountService antiPatternsPatternCountService;
+    private Integer fixturePatternId = 313370815;
+    private Integer fixtureSeedValue = 4711;
+
+    public AntiPatternsPatternCountServiceTest()
+    {
+        super(AntiPatternsMainScreen.class);
+    }
+
+    @Before
+    public void setUp() throws Exception{
+        try {
+            super.setUp();
+        } catch(Exception e) {
+            System.out.print("complain loudly");
+            throw e;
+        }
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        myActivity = getActivity();
+        setUpSharedPreferences();
+        antiPatternsPatternCountService = new AntiPatternsPatternCountService();
+        antiPatternsPatternCountService.init(myActivity);
+    }
+
+
+    @Test
+    public void readCounter_ReturnsExpectedValue()
+    {
+        Integer expectedCounter = antiPatternsPatternCountService.readCounter(fixturePatternId);
+
+        assertEquals(expectedCounter, fixtureSeedValue);
+    }
+
+    @Test
+    public void resetCounter_clearsValue()
+    {
+        antiPatternsPatternCountService.resetCounter(fixturePatternId);
+        Integer expectedCounter = antiPatternsPatternCountService.readCounter(fixturePatternId);
+        assertEquals(expectedCounter, (Integer) 0);
+
+    }
+
+
+    private void setUpSharedPreferences() {
+        mySharedPreferences = myActivity.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mySharedPreferences.edit();
+        editor.putInt(fixturePatternId.toString(), fixtureSeedValue);
+        editor.commit();
+    }
+}
